48c1b406f6a2499e68dfcafa83488b66fee76bab
sogyeokdong9 <101911603+sogyeokdong9@users.noreply.github.com>
2022-03-24 20:51:46 +0900

    fix: Radio button UI improvement
    라디오 버튼을 checked 했음에도 UI에 직접 반영하는게 아니라,
    form을 submit했을 때, 결과를 반영했다.
    사용자의 의도에 맞지 않는 것 같아서,
    이를 보완했으며 style, script는 추가 개선 작업 필요함.

--- components/button/size/script/script.js
+++ components/button/size/script/script.js
@@ -4,14 +4,58 @@
   const form = document.querySelector("form");
   const cntnrElmn = document.querySelector('.wrap');
   const btnElmn = cntnrElmn.querySelectorAll('button[type="button"]');
-  const orgnlBtcln = [];
-  for (let i = 0; i < btnElmn.length; i++) {
-    const element = btnElmn[i];
-    orgnlBtcln.push(element.className);
+  const cntnrRdoLbl = document.querySelectorAll('.otter-radio-wrapper');
+  const cntnrChckbxLbl = document.querySelector('.otter-checkbox-wrapper');
+
+  for (let i = 0; i < cntnrRdoLbl.length; i++) {
+    const element = cntnrRdoLbl[i];
+    element.addEventListener("click", function() {
+      deleteChecked();
+      addChecked(i);
+    })
   }
-  // console.log('Original button class name: ' + orgnlBtcln);
-  form.addEventListener('submit', function(event) {
+
+  function deleteChecked() {
     'use strict';
+    for (let i = 0; i < cntnrRdoLbl.length; i++) {
+      const element = cntnrRdoLbl[i];
+      element.classList.remove('otter-radio-wrapper-checked');
+      element.firstElementChild.classList.remove('otter-radio-checked');
+      element.firstElementChild.children[0].removeAttribute('checked');
+    }
+  }
+
+  function addChecked(idx) {
+    'use strict';
+    const element = cntnrRdoLbl[idx];
+    element.classList.add('otter-radio-wrapper-checked');
+    element.firstElementChild.classList.add('otter-radio-checked');
+    element.firstElementChild.children[0].setAttribute('checked', 'checked');
+  }
+
+
+  cntnrChckbxLbl.addEventListener("click", toggleCheckBox);
+  
+  function toggleCheckBox() {
+    'use strict';
+    const isChecked = document.querySelector('.otter-checkbox input[type="checkbox"]').checked;
+    const spanElmn = cntnrChckbxLbl.querySelector('.otter-checkbox');
+    const checkElmn = spanElmn.querySelector('.otter-checkbox-input');
+    let result; 
+    if(isChecked) {
+      spanElmn.classList.toggle('otter-checkbox-checked');
+      checkElmn.toggleAttribute('checked');
+      result = isChecked;
+    } else {
+      spanElmn.classList.remove('otter-checkbox-checked');
+      checkElmn.removeAttribute('checked');
+      result = isChecked;
+    }
+    return result;
+  }
+
+  function submitLog() {
+    'use strict'
     const logElmn = cntnrElmn.querySelector('.event-log');
     const time = new Date();
     const timeStr = time.toLocaleTimeString();
@@ -23,52 +67,24 @@
     logElmn.append(newItmElmn);
     const cntSbmt = logElmn.querySelectorAll('li').length - 1;
     const prvClsNm = logElmn.querySelectorAll('li')[cntSbmt - 1].outerText;
-    const trnClsNm = prvClsNm.split(',', 2);
-    // 'Node list(li): '
-    // console.log(logElmn.querySelectorAll('li')); 
-    // console.log('Previous class name: ' + prvClsNm);
-    // console.log('Event log split: ' + trnClsNm);
-    const cntrRdoGp = cntnrElmn.querySelector('.otter-radio-group');
-    const lbElmn = cntrRdoGp.querySelectorAll('.otter-radio-wrapper');
-    const spElmn = cntrRdoGp.querySelectorAll('.otter-radio');
-    const rdoElmn = cntrRdoGp.querySelectorAll('input[type="radio"]');
-    const orgnRdcln = [];
-    for (let i = 0; i < lbElmn.length; i++) {
-      const element = lbElmn[i];
-      orgnRdcln.push(element.className);
-    }
-    // console.log('Original label class name: ' + orgnRdcln);
-    for (let i = 0; i < lbElmn.length; i++) {
-      const eLabel = lbElmn[i], eSpan = spElmn[i], eRadio = rdoElmn[i];
-      eLabel.classList.remove('otter-radio-wrapper-checked')
-      eSpan.classList.remove('otter-radio-checked')
-      eRadio.removeAttribute('checked');
-    }
+    const trnClsNm = prvClsNm.split(',', 2)[0];
+  }
+  
+  form.addEventListener('submit', function(event) {
+    'use strict';
+    submitLog();
+    const chckdElmn = form.querySelector('input[name="btn-size"]:checked');
+    const chckdVal = chckdElmn.value;
     for (let i = 0; i < btnElmn.length; i++) {
       const element = btnElmn[i];
-      // console.log(element);
-      // console.log(btnElmn[i].className);
-      btnElmn[i].classList.remove(trnClsNm[0]);
-      btnElmn[i].classList.add(chckdVal);
-      // console.log(trnClsNm[0]);
-      if (chckdVal === 'otter-btn-sm') {
-        addClsAttr(lbElmn[0], spElmn[0], rdoElmn[0], 'otter-radio-wrapper-checked', 'otter-radio-checked', 'checked')
-      } else if (chckdVal === 'otter-btn-df') {
-        addClsAttr(lbElmn[1], spElmn[1], rdoElmn[1], 'otter-radio-wrapper-checked', 'otter-radio-checked', 'checked')
-      } else if (chckdVal === 'otter-btn-lg') {
-        addClsAttr(lbElmn[2], spElmn[2], rdoElmn[2], 'otter-radio-wrapper-checked', 'otter-radio-checked', 'checked')
+      element.classList.remove('otter-btn-sm', 'otter-btn-df', 'otter-btn-lg');
+      element.classList.add(chckdVal);
+      if (toggleCheckBox()) {
+        element.classList.remove('otter-btn-dangerous');
+        element.classList.add('otter-btn-dangerous');  
       } else {
-        addClsAttr(lbElmn[1], spElmn[1], rdoElmn[1], 'otter-radio-wrapper-checked', 'otter-radio-checked', 'checked')
+        element.classList.remove('otter-btn-dangerous');
       }
-      function addClsAttr(t1, t2, t3, c1, c2, a1) {
-        'use strict'
-        t1.classList.add(c1);
-        t2.classList.add(c2);
-        t3.setAttribute(a1, a1);
-      }
-      // lbElmn[0].classList.add('otter-radio-wrapper-checked');
-      // spElmn[0].classList.add('otter-radio-checked');
-      // rdoElmn[0].setAttribute('checked', 'checked');
     }
     event.preventDefault();
   }, false);
