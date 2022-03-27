082a11a3454d97dfa7efb9c1fb601a79edf6c874
sogyeokdong9 <101911603+sogyeokdong9@users.noreply.github.com>
2022-03-27 20:39:33 +0900

    feat: Add Component - Radio-Group
    - Improving scss file structure
    - Reflect additional scripts by adding components
    - Improvement script code

---
+++ components/radio/radio-group/script/script.js
@@ -0,0 +1,50 @@
+'use strict';
+(function () {
+  'use strict';
+  const cntnrElmnt = document.querySelector('.wrap');
+  const radioLbls = cntnrElmnt.querySelectorAll('.otter-radio-wrapper');
+  const radioName = [];
+
+  for (let i = 0; i < radioLbls.length; i++) {
+    const element = radioLbls[i];
+    const spanElmn = element.querySelector('.otter-radio');
+    const checkElmn = spanElmn.querySelector('.otter-radio-input');
+    radioName.push(checkElmn.name);
+    element.addEventListener("click", function() {
+      if(allEqual(radioName)) {
+        unchckAll();
+      }
+      addCheck(i);
+    })
+  }
+
+  const allEqual = arr => arr.every( v => v === arr[0] );
+
+  function unchckAll() {
+    'use strict';
+    for (let i = 0; i < radioLbls.length; i++) {
+      const element = radioLbls[i];
+      if (!element.hasAttribute('disabled') && !element.classList.contains('otter-radio-wrapper-disabled')) {
+        element.classList.remove('otter-radio-wrapper-checked');
+        element.firstElementChild.classList.remove('otter-radio-checked');
+        element.firstElementChild.children[0].removeAttribute('checked');
+      }
+    }
+  }
+  function addCheck(idx) {
+    'use strict';
+    const element = radioLbls[idx];
+    if (!element.hasAttribute('disabled') && !element.classList.contains('otter-radio-wrapper-disabled')) {
+      element.classList.add('otter-radio-wrapper-checked');
+      element.firstElementChild.classList.add('otter-radio-checked');
+      element.firstElementChild.children[0].setAttribute('checked', 'checked');
+    }
+  }
+  function addDisabled(idx) {
+    'use strict';
+    const element = radioLbls[idx];
+    element.classList.toggle('otter-radio-wrapper-disabled');
+    element.firstElementChild.classList.toggle('otter-radio-disabled');
+    element.firstElementChild.children[0].toggleAttribute('disabled', 'disabled');
+  }
+})();
