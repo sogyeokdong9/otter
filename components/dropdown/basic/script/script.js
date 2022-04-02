4df973bfc61fe73f59dba189a67bdb1e1aaec740
sogyeokdong9 <101911603+sogyeokdong9@users.noreply.github.com>
2022-04-02 16:47:05 +0900

    refactor: Mouse event log & SCSS(Improve Structure)
    All clear. 🤓

--- components/dropdown/basic/script/script.js
+++ components/dropdown/basic/script/script.js
@@ -5,30 +5,48 @@
   const trgElmn = cntnrElmnt.querySelector('.otter-dropdown-trigger');
   const drpdnMn = document.querySelector('.otter-dropdown');
 
+  function toStringTime() {
+    'use strict'
+    let result;
+    const time = new Date();
+    const timeStr = time.toLocaleTimeString();
+    return result = timeStr;
+  }
+
+  function mouseEventLog(mouseState) {
+    'use strict'
+    const logElmnt = cntnrElmnt.querySelector('.event-log');
+    const newItmElmn = document.createElement('li');
+    const newCntnt = document.createTextNode( mouseState + ': ' + toStringTime() + ' ' + drpdnMn.className );
+    newItmElmn.append(newCntnt);
+    logElmnt.append(newItmElmn);
+  }
+
   function throwScrnDrpdnMn() {
+    'use strict'
     drpdnMn.style.minWidth = (trgElmn.offsetWidth) + 'px';
     drpdnMn.style.left = '-' + (document.body.clientWidth) + 'px';
     drpdnMn.style.top = '-' + (document.body.clientHeight) + 'px';
   }
 
   throwScrnDrpdnMn();
-
-  trgElmn.addEventListener('mousemove', function() {
-    console.log(drpdnMn.className);
-    drpdnMn.classList.replace("otter-dropdown-hidden", "otter-dropdown-see");
-    trgElmn.classList.add('otter-dropdown-open');
+  // previousElementSibling
+  trgElmn.addEventListener('mouseover', function() {
+    'use strict'
+    drpdnMn.classList.replace("otter-dropdown-hidden", "otter-dropdown-visible");
+    trgElmn.classList.replace("otter-dropdown-close", "otter-dropdown-open");
+    trgElmn.lastChild.previousElementSibling.ariaExpanded = true;
     drpdnMn.style.minWidth = (trgElmn.offsetWidth) + 'px';
     drpdnMn.style.left = (trgElmn.offsetLeft) + 'px';
     drpdnMn.style.top = (trgElmn.offsetTop + trgElmn.offsetHeight) + 'px';
+    mouseEventLog('mouseover');
   })
   drpdnMn.addEventListener('mouseleave', function() {
-    console.log(drpdnMn.className);
-    drpdnMn.classList.replace("otter-dropdown-see", "otter-dropdown-hidden");
-    trgElmn.classList.remove('otter-dropdown-open');
+    'use strict'
+    trgElmn.lastChild.previousElementSibling.ariaExpanded = false;
+    drpdnMn.classList.replace("otter-dropdown-visible", "otter-dropdown-hidden");
+    trgElmn.classList.replace("otter-dropdown-open", "otter-dropdown-close");
+    mouseEventLog('mouseleave');
     throwScrnDrpdnMn();
   })
-
-
-
-
 })();
