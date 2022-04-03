c3d41c344bb240bba4c2421b105a7caa6e6c2376
sogyeokdong9 <101911603+sogyeokdong9@users.noreply.github.com>
2022-04-03 10:09:29 +0900

    refactor: Mouse event log & SCSS(Improve Structure)
    - Delete duplicated code.
    - Refactor: mouseEventLog
    - Delete function: throwScrnDrpdnMn()
    - Add function: setLctDrpdnMn(), isDrpdnMnOpen()

--- components/dropdown/basic/script/script.js
+++ components/dropdown/basic/script/script.js
@@ -6,7 +6,7 @@
   const drpdnMn = document.querySelector('.otter-dropdown');
 
   function toStringTime() {
-    'use strict'
+    'use strict';
     let result;
     const time = new Date();
     const timeStr = time.toLocaleTimeString();
@@ -14,39 +14,53 @@
   }
 
   function mouseEventLog(mouseState) {
-    'use strict'
+    'use strict';
     const logElmnt = cntnrElmnt.querySelector('.event-log');
     const newItmElmn = document.createElement('li');
-    const newCntnt = document.createTextNode( mouseState + ': ' + toStringTime() + ' ' + drpdnMn.className );
-    newItmElmn.append(newCntnt);
+    const newItmSpn1 = document.createElement('span');
+    const newItmSpn2 = document.createElement('span');
+    const newItmSpn3 = document.createElement('span');
+    const newCntnt1 = document.createTextNode( mouseState );
+    const newCntnt2 = document.createTextNode( toStringTime() );
+    const newCntnt3 = document.createTextNode( drpdnMn.className );
+    newItmElmn.classList.add('log-item');
+    newItmSpn1.classList.add('specified');
+    newItmSpn2.classList.add('log-time');
+    newItmSpn3.classList.add('changed-class');
+    newItmSpn1.append(newCntnt1);
+    newItmSpn2.append(newCntnt2);
+    newItmSpn3.append(newCntnt3);
+    newItmElmn.append(newItmSpn1);
+    newItmElmn.append(newItmSpn2);
+    newItmElmn.append(newItmSpn3);
     logElmnt.append(newItmElmn);
   }
 
-  function throwScrnDrpdnMn() {
-    'use strict'
+  function setLctDrpdnMn(sign) {
     drpdnMn.style.minWidth = (trgElmn.offsetWidth) + 'px';
-    drpdnMn.style.left = '-' + (document.body.clientWidth) + 'px';
-    drpdnMn.style.top = '-' + (document.body.clientHeight) + 'px';
+    drpdnMn.style.left = (sign === '-') ? '-' + (document.body.clientWidth) + 'px' : (trgElmn.offsetLeft) + 'px';
+    drpdnMn.style.top = (sign === '-') ? '-' + (document.body.clientHeight) + 'px' : (trgElmn.offsetTop + trgElmn.offsetHeight) + 'px';
   }
 
-  throwScrnDrpdnMn();
-  // previousElementSibling
+  function isDrpdnMnOpen(bool, clsNm1, clsNm2, clsNm3, clsNm4) {
+    'use strict';
+    trgElmn.lastChild.previousElementSibling.ariaExpanded = bool;
+    drpdnMn.classList.replace(clsNm1, clsNm2);
+    trgElmn.classList.replace(clsNm3, clsNm4);
+  }
+
+  setLctDrpdnMn('-');
+
   trgElmn.addEventListener('mouseover', function() {
-    'use strict'
-    drpdnMn.classList.replace("otter-dropdown-hidden", "otter-dropdown-visible");
-    trgElmn.classList.replace("otter-dropdown-close", "otter-dropdown-open");
-    trgElmn.lastChild.previousElementSibling.ariaExpanded = true;
-    drpdnMn.style.minWidth = (trgElmn.offsetWidth) + 'px';
-    drpdnMn.style.left = (trgElmn.offsetLeft) + 'px';
-    drpdnMn.style.top = (trgElmn.offsetTop + trgElmn.offsetHeight) + 'px';
+    'use strict';
+    isDrpdnMnOpen(true, "otter-dropdown-hidden", "otter-dropdown-visible", "otter-dropdown-close", "otter-dropdown-open");
+    setLctDrpdnMn('+');
     mouseEventLog('mouseover');
   })
   drpdnMn.addEventListener('mouseleave', function() {
     'use strict'
-    trgElmn.lastChild.previousElementSibling.ariaExpanded = false;
-    drpdnMn.classList.replace("otter-dropdown-visible", "otter-dropdown-hidden");
-    trgElmn.classList.replace("otter-dropdown-open", "otter-dropdown-close");
+    isDrpdnMnOpen(false, "otter-dropdown-visible", "otter-dropdown-hidden", "otter-dropdown-open", "otter-dropdown-close");
+    setLctDrpdnMn('-');
     mouseEventLog('mouseleave');
-    throwScrnDrpdnMn();
   })
 })();
