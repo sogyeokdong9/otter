93fec38fc4b1f575dc6537043b18b67aa1f93555
sogyeokdong9 <101911603+sogyeokdong9@users.noreply.github.com>
2022-04-06 23:53:31 +0900

    refactor: Mouse event log & SCSS(Improve Structure)
    Todo List
    - Window resize
    - Refactor: mouseEventLog
    - Optimizing source code

--- components/dropdown/placement/script/script.js
+++ components/dropdown/placement/script/script.js
@@ -5,12 +5,15 @@
   const trgElmn = cntnrElmnt.querySelectorAll('.otter-dropdown-trigger');
   const drpdnMn = document.querySelectorAll('.otter-dropdown');
 
+  console.log(trgElmn);
+
   for (let i = 0; i < trgElmn.length; i++) {
     const element = trgElmn[i];
     element.addEventListener('mouseenter', function() {
       'use strict';
       openDrpdnMn(i);
       mouseEventLog('mouseenter')
+      recordLog();
     })
     for (let i = 0; i < drpdnMn.length; i++) {
       const element = drpdnMn[i];
@@ -28,8 +31,12 @@
       closeAllDrpdnMn(i);
     }
     const element = trgElmn[idx];
+    const prefixPlcmnt = 'otter-dropdown-placement-';
+    const positionPlcmnt = element.getAttribute('data-placement');
+    const makePlcmntCls = prefixPlcmnt + positionPlcmnt;
     element.lastChild.previousElementSibling.ariaExpanded = true;
     element.classList.replace('otter-dropdown-close', 'otter-dropdown-open');
+    drpdnMn[idx].classList.add(makePlcmntCls);
     drpdnMn[idx].classList.replace("otter-dropdown-hidden", "otter-dropdown-visible");
     setLctDrpdnMn('in', idx);
   }
@@ -64,19 +71,19 @@
     const ALIGN_TOP_AXIS = (ALIGN_BTM_AXIS - drpdnMn[idx].offsetHeight - trgElmn[idx].offsetHeight);
     const SCROLL_Y = Math.round(window.scrollY);
 
-    function setPlacement( val, param) {
+    function setPlacement( val, para ) {
       'use strict';
       const alignTopBasis = ( ALIGN_TOP_AXIS > 0 && ALIGN_TOP_AXIS > SCROLL_Y );
       const alignBtmBasis = ( ALIGN_BTM_AXIS > 0 && ALIGN_BTM_AXIS > SCROLL_Y );
       if (btnPlacement === val) {
         drpdnMn[idx].style.minWidth = (trgElmn[idx].offsetWidth) + 'px';
-        drpdnMn[idx].style.left = (loc === 'out') ? '-' + CLIENT_WIDTH + 'px' : param + 'px';
+        drpdnMn[idx].style.left = (loc === 'out') ? '-' + CLIENT_WIDTH + 'px' : para + 'px';
         if (btnPlacement === 'topLeft' || btnPlacement === 'top' || btnPlacement === 'topRight') {
-          defineAlignBasis( alignTopBasis, ALIGN_TOP_AXIS, ALIGN_BTM_AXIS)
+          defineAlignBasis(alignTopBasis, ALIGN_TOP_AXIS, ALIGN_BTM_AXIS)
         } else {
-          defineAlignBasis( alignBtmBasis, ALIGN_BTM_AXIS, ALIGN_TOP_AXIS)
+          defineAlignBasis(alignBtmBasis, ALIGN_BTM_AXIS, ALIGN_TOP_AXIS)
         }
-        function defineAlignBasis( axis, val1, val2) {
+        function defineAlignBasis(axis, val1, val2) {
           if(axis) {
             drpdnMn[idx].style.top = (loc === 'out') ? '-' + CLIENT_HEIGHT + 'px' : val1 + 'px';
           } else {
@@ -84,8 +91,7 @@
           } 
         }
       }
-    }
-    
+    }  
     setPlacement('topLeft', JUST_LEFT_AXIS);
     setPlacement('top', JUST_CENTER_AXIS);
     setPlacement('topRight', JUST_RIGHT_AXIS);
@@ -117,16 +123,43 @@
     return isDrpdnMn = activeElmn;
   }
 
+  function recordLog() {
+    const arrLog = [];
+    const getLog = cntnrElmnt.querySelectorAll('.log-item');
+    arrLog.push(getLog);
+    
+    const eventLog = cntnrElmnt.querySelector('.event-log');
+    if (getLog.length > 1) {
+      const latestLog = eventLog.lastChild;
+      const prelatestLog = latestLog.previousSibling;
+      let latestActvElmn = latestLog.childNodes[2].innerText;
+      let prelatestActvElmn = prelatestLog.childNodes[2].innerText;
+      // console.log(latestActvElmn, prelatestActvElmn);
+      return prelatestActvElmn;
+    }
+    // if (latestLog) {
+    //   prelatestLog = latestLog.previousSibling;
+    //   console.log(prelatestLog);
+    // }
+    // let latestActvElmn = latestLog.childNodes[2].innerText;
+    // console.log(getLog);
+    // console.log(latestActvElmn);
+    // console.log(latestLog);
+    // console.log(latestLog.childNodes[2].innerText);
+    // console.log(latestLog.innerText);
+    // console.log(arrLog);
+    // console.log(arrLog[arrLog.length - 1]);
+    // console.log('last log: ' + getLog[(getLog.length-1)]);
+  }
+
   function mouseEventLog(mouseState) {
     'use strict';
-    const loadBtnItemText = getActiveBtnElmn().firstElementChild.outerText;
+    const loadBtnItemText = getActiveBtnElmn().firstElementChild.innerText;
     const loadBtnItemAttr = getActiveBtnElmn().getAttribute('data-placement');
     const loadBtnItemCls = getActiveBtnElmn().classList[4];
     const loadBtnItemIdx = getActiveBtnElmn().getAttribute('data-index-number');
-
     const loadDrpdnMnItemText = getActiveDrpdnMn().innerText;
     const loadDrpdnMnItemIdx = getActiveDrpdnMn().getAttribute('data-index-number');
-
     const logElmnt = cntnrElmnt.querySelector('.event-log');
     const newItmElmn = document.createElement('li');
     const newItmSpn1 = document.createElement('span');
@@ -139,21 +172,21 @@
     const newItmSpn8 = document.createElement('span');
     const newCntnt1 = document.createTextNode( mouseState );
     const newCntnt2 = document.createTextNode( toStringTime() );
-    const newCntnt3 = document.createTextNode( loadBtnItemText );
-    const newCntnt4 = document.createTextNode( loadBtnItemAttr );
-    const newCntnt5 = document.createTextNode( loadBtnItemCls );
-    const newCntnt6 = document.createTextNode( loadBtnItemIdx );
-    const newCntnt7 = document.createTextNode( loadDrpdnMnItemText );
-    const newCntnt8 = document.createTextNode( loadDrpdnMnItemIdx );
+    const newCntnt3 = document.createTextNode( loadBtnItemIdx );
+    const newCntnt4 = document.createTextNode( loadBtnItemText );
+    const newCntnt5 = document.createTextNode( loadBtnItemAttr );
+    const newCntnt6 = document.createTextNode( loadBtnItemCls );
+    const newCntnt7 = document.createTextNode( loadDrpdnMnItemIdx );
+    const newCntnt8 = document.createTextNode( loadDrpdnMnItemText );
     newItmElmn.classList.add('log-item');
     newItmSpn1.classList.add('specified');
     newItmSpn2.classList.add('log-time');
-    newItmSpn3.classList.add('trigger-txt');
-    newItmSpn4.classList.add('trigger-plc');
-    newItmSpn5.classList.add('trigger-cls');
-    newItmSpn6.classList.add('trigger-idx');
-    newItmSpn7.classList.add('dropdown-txt');
-    newItmSpn8.classList.add('dropdown-idx');
+    newItmSpn3.classList.add('trigger-idx');
+    newItmSpn4.classList.add('trigger-txt');
+    newItmSpn5.classList.add('trigger-plc');
+    newItmSpn6.classList.add('trigger-cls');
+    newItmSpn7.classList.add('dropdown-idx');
+    newItmSpn8.classList.add('dropdown-txt');
     newItmSpn1.append(newCntnt1);
     newItmSpn2.append(newCntnt2);
     newItmSpn3.append(newCntnt3);
@@ -172,5 +205,5 @@
     newItmElmn.append(newItmSpn8);
     logElmnt.append(newItmElmn);
   }
-
 })();
+
