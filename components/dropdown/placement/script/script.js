dcc35f3e9d0d0987855f1dd4a1c9148ac36260a1
sogyeokdong9 <101911603+sogyeokdong9@users.noreply.github.com>
2022-04-10 22:59:52 +0900

    refactor: createElement
    'makeHtmlElement' use to make 'HTML Element' 🧐

--- components/dropdown/placement/script/script.js
+++ components/dropdown/placement/script/script.js
@@ -13,7 +13,6 @@
       mouseEventLog('mouseenter')
       recordLog();
     })
-    // TODO: Get the previous mouseenter data, and close the previous dropdown menu.
     for (let i = 0; i < drpdnMn.length; i++) {
       const element = drpdnMn[i];
       element.addEventListener('mouseleave', function() {
@@ -26,9 +25,8 @@
 
   function openDrpdnMn(idx) {
     'use strict';
-    // TODO: Get the previous mouseenter data, and close the previous dropdown menu.
     for (let i = 0; i < drpdnMn.length; i++) {
-      closeAllDrpdnMn(i);
+      closeDrpdnMn(i);
     }
     const element = trgElmn[idx];
     const hasAttrPlcmnt = element.hasAttribute('data-placement');
@@ -48,45 +46,17 @@
     if (bool) {
       openDrpdnMn(idx);
     } else {
-      closeAllDrpdnMn(idx);
+      closeDrpdnMn(idx);
     }
   }
 
-  function closeAllDrpdnMn(idx) {
+  function closeDrpdnMn(idx) {
     'use strict';
     trgElmn[idx].lastChild.previousElementSibling.ariaExpanded = false;
     drpdnMn[idx].classList.replace("otter-dropdown-visible", "otter-dropdown-hidden");
     trgElmn[idx].classList.replace("otter-dropdown-open", "otter-dropdown-close");
     setLctDrpdnMn('out', idx)
   }
-
-  window.addEventListener('resize', function(){
-    'use strict';
-    console.log('resize event!');
-    if( getActiveDrpdnMn() ) {
-      const JUST_LEFT_AXIS = (getActiveBtnElmn().offsetLeft);
-      const JUST_CENTER_AXIS = ((getActiveBtnElmn().offsetLeft) + ( (getActiveBtnElmn().offsetWidth) - getActiveDrpdnMn().offsetWidth) / 2);
-      const JUST_RIGHT_AXIS = ((getActiveBtnElmn().offsetLeft) + ( (getActiveBtnElmn().offsetWidth) - getActiveDrpdnMn().offsetWidth));
-
-      function defineBaseCoordinate(val) {
-        'use strict';
-        getActiveDrpdnMn().style.left = val + 'px';
-      } 
-      if ( getActiveDrpdnMn().classList.contains('otter-dropdown-placement-default') || 
-        getActiveDrpdnMn().classList.contains('otter-dropdown-placement-bottomLeft') || 
-        getActiveDrpdnMn().classList.contains('otter-dropdown-placement-topLeft') ) {
-        defineBaseCoordinate(JUST_LEFT_AXIS);
-      } else if ( getActiveDrpdnMn().classList.contains('otter-dropdown-placement-top') ||
-        getActiveDrpdnMn().classList.contains('otter-dropdown-placement-bottom') ) {
-        defineBaseCoordinate(JUST_CENTER_AXIS);
-      } else if ( getActiveDrpdnMn().classList.contains('otter-dropdown-placement-bottomRight') ||
-        getActiveDrpdnMn().classList.contains('otter-dropdown-placement-topRight' )) {
-        defineBaseCoordinate(JUST_RIGHT_AXIS);
-      } else {
-        defineBaseCoordinate(JUST_LEFT_AXIS);
-      }
-    }
-  });
 
   function setLctDrpdnMn(loc, idx) {
     'use strict';
@@ -159,10 +129,7 @@
     const logPreElmArry = [];
     const getLog = cntnrElmnt.querySelectorAll('.log-item');
     logMsEvnt.push(getLog)
-    // const eventLog = cntnrElmnt.querySelector('.event-log');
-    // const latestLog = eventLog.lastChild;
-    // const CURRENT_ACTIVE_ELEMENT = latestLog.childNodes[2].innerText;
-    // const PREVIOUS_ELEMENT = (getLog.length > 1) ? latestLog.previousSibling.childNodes[2].innerText : null;
+
     for (let i = 0; i < getLog.length; i++) {
       const element = getLog[i];
       logActElmArry.push(element.childNodes[2].outerText);
@@ -171,13 +138,12 @@
       const element = logActElmArry[i];
       logPreElmArry[i] = element;
     }
-    logPreElmArry.unshift( null );
+    logPreElmArry.unshift( '-' );
     logPreElmArry.pop();
     for (let i = 0; i < getLog.length; i++) {
       const element = getLog[i];
       element.lastChild.textContent = logPreElmArry[i];
     }
-    // return [logActElmArry, logPreElmArry];
   }
 
   function mouseEventLog(mouseState) {
@@ -189,53 +155,67 @@
     const loadDrpdnMnItemText = getActiveDrpdnMn().innerText.replace(/\n\r?/g, '/');
     const loadDrpdnMnItemIdx = getActiveDrpdnMn().getAttribute('data-index-number');
     const logElmnt = cntnrElmnt.querySelector('.event-log');
-    const newItmElmn = document.createElement('li');
-    const newItmSpn1 = document.createElement('span');
-    const newItmSpn2 = document.createElement('span');
-    const newItmSpn3 = document.createElement('span');
-    const newItmSpn4 = document.createElement('span');
-    const newItmSpn5 = document.createElement('span');
-    // const newItmSpn6 = document.createElement('span');
-    const newItmSpn7 = document.createElement('span');
-    const newItmSpn8 = document.createElement('span');
-    const newItmSpn9 = document.createElement('span');
-    const newCntnt1 = document.createTextNode( mouseState );
-    const newCntnt2 = document.createTextNode( toStringTime() );
-    const newCntnt3 = document.createTextNode( loadBtnItemIdx );
-    const newCntnt4 = document.createTextNode( loadBtnItemText );
-    const newCntnt5 = document.createTextNode( loadBtnItemAttr );
-    // const newCntnt6 = document.createTextNode( loadBtnItemCls );
-    const newCntnt7 = document.createTextNode( loadDrpdnMnItemIdx );
-    const newCntnt8 = document.createTextNode( loadDrpdnMnItemText );
-    const newCntnt9 = document.createTextNode( 'Data is from recordLog()' );
-    newItmElmn.classList.add('log-item');
-    newItmSpn1.classList.add('specified');
-    newItmSpn2.classList.add('log-time');
-    newItmSpn3.classList.add('trigger-idx');
-    newItmSpn4.classList.add('trigger-txt');
-    newItmSpn5.classList.add('trigger-plc');
-    // newItmSpn6.classList.add('trigger-cls');
-    newItmSpn7.classList.add('dropdown-idx');
-    newItmSpn8.classList.add('dropdown-txt');
-    newItmSpn9.classList.add('previous-idx');
-    newItmSpn1.append(newCntnt1);
-    newItmSpn2.append(newCntnt2);
-    newItmSpn3.append(newCntnt3);
-    newItmSpn4.append(newCntnt4);
-    newItmSpn5.append(newCntnt5);
-    // newItmSpn6.append(newCntnt6);
-    newItmSpn7.append(newCntnt7);
-    newItmSpn8.append(newCntnt8);
-    newItmSpn9.append(newCntnt9);
-    newItmElmn.append(newItmSpn1);
-    newItmElmn.append(newItmSpn2);
-    newItmElmn.append(newItmSpn3);
-    newItmElmn.append(newItmSpn4);
-    newItmElmn.append(newItmSpn5);
-    // newItmElmn.append(newItmSpn6);
-    newItmElmn.append(newItmSpn7);
-    newItmElmn.append(newItmSpn8);
-    newItmElmn.append(newItmSpn9);
-    logElmnt.append(newItmElmn);
+    // const groupOfItems = [ mouseState, toStringTime(), loadBtnItemIdx, loadBtnItemText, loadBtnItemAttr, loadBtnItemCls, loadDrpdnMnItemIdx, loadDrpdnMnItemText, 'Data is from recordLog()' ];
+    // const groupOfClass = [ 'specified', 'log-time', 'trigger-idx', 'trigger-txt', 'trigger-plc', 'trigger-cls', 'dropdown-idx', 'dropdown-txt', 'previous-idx' ];
+    
+    const makeHtmlElement = function (tagName, ...attr) {
+      const element = document.createElement(tagName);
+      for (let prop of attr) {
+        const [key, value] = Object.entries(prop)[0];
+        if (key == 'textContent' || key == 'innerText') {
+          element.textContent = value;
+        } else {
+          element.setAttribute(key, value);
+        }
+      }
+      return element;
+    };
+
+    const itemContainer = makeHtmlElement('li', { class: 'log-item' });
+    const groopOfPairs = [ 
+      { key: 1, value: mouseState, class: 'specified' },
+      { key: 2, value: toStringTime(), class: 'log-time' },
+      { key: 3, value: loadBtnItemIdx, class: 'trigger-idx' },
+      { key: 4, value: loadBtnItemText, class: 'trigger-txt' },
+      { key: 5, value: loadBtnItemAttr, class: 'trigger-plc' },
+      { key: 6, value: loadBtnItemCls, class: 'trigger-cls' },
+      { key: 7, value: loadDrpdnMnItemIdx, class: 'dropdown-idx' },
+      { key: 8, value: loadDrpdnMnItemText, class: 'dropdown-txt' },
+      { key: 9, value: 'Data is from recordLog()', class: 'previous-idx' }
+    ]
+    const [item1, item2, item3, item4, item5, item6, item7, item8, item9] = groopOfPairs.map((item) =>
+      makeHtmlElement('span', { class: item.class }, { textContent: item.value })
+    );
+    itemContainer.append(item1, item2, item3, item4, item5, item6, item7, item8, item9);
+    logElmnt.append(itemContainer);
   }
+
+  window.addEventListener('resize', function(){
+    'use strict';
+    console.log('resize event!');
+    if( getActiveDrpdnMn() ) {
+      const JUST_LEFT_AXIS = (getActiveBtnElmn().offsetLeft);
+      const JUST_CENTER_AXIS = ((getActiveBtnElmn().offsetLeft) + ( (getActiveBtnElmn().offsetWidth) - getActiveDrpdnMn().offsetWidth) / 2);
+      const JUST_RIGHT_AXIS = ((getActiveBtnElmn().offsetLeft) + ( (getActiveBtnElmn().offsetWidth) - getActiveDrpdnMn().offsetWidth));
+
+      function defineBaseCoordinate(val) {
+        'use strict';
+        getActiveDrpdnMn().style.left = val + 'px';
+      } 
+      if ( getActiveDrpdnMn().classList.contains('otter-dropdown-placement-default') || 
+        getActiveDrpdnMn().classList.contains('otter-dropdown-placement-bottomLeft') || 
+        getActiveDrpdnMn().classList.contains('otter-dropdown-placement-topLeft') ) {
+        defineBaseCoordinate(JUST_LEFT_AXIS);
+      } else if ( getActiveDrpdnMn().classList.contains('otter-dropdown-placement-top') ||
+        getActiveDrpdnMn().classList.contains('otter-dropdown-placement-bottom') ) {
+        defineBaseCoordinate(JUST_CENTER_AXIS);
+      } else if ( getActiveDrpdnMn().classList.contains('otter-dropdown-placement-bottomRight') ||
+        getActiveDrpdnMn().classList.contains('otter-dropdown-placement-topRight' )) {
+        defineBaseCoordinate(JUST_RIGHT_AXIS);
+      } else {
+        defineBaseCoordinate(JUST_LEFT_AXIS);
+      }
+    }
+  });
+
 })();
