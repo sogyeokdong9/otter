5364ca562ab846edb55ddd07745cb82272244d3d
sogyeokdong9 <101911603+sogyeokdong9@users.noreply.github.com>
2022-04-28 07:09:10 +0900

    feat: Changed script & scss file
    Add Component - Tooltip(Colourful tooltip)
    
    🍏 f4baaff
    
    🐹 91994b2
    🐸 389c750
    🐯 2cb6dda
    ☃️ d7ddfeb

--- components/dropdown/basic/script/createDropdownMenu.js
+++ components/dropdown/basic/script/createDropdownMenu.js
@@ -5,6 +5,7 @@
     {
       "id":"1",
       "name":"Dasboard",
+      "dataTheme":"yellow",
       "menu":[
         {
           "id":"dropdown-menu-ul-li-tmp-key-0-0",
@@ -40,22 +41,20 @@
     };
     const container = makeHtmlElement(
       'div', 
-      { style: 'position: absolute; top: 0px; left: 0px; width: 100%;' }
+      { style: 'position: absolute; top: 0; left: 0; width: 100%;' }
     );
     const wrapper = makeHtmlElement(
       'div', 
-      { class: 'otter-dropdown otter-dropdown-visible' },
-      { style: 'min-width: 98px; left: -136px; top: -188px;' }
-      // { dataIndexNumber: items[order].id }
+      { class: `otter-dropdown otter-dropdown-show-arrow-${items[order].dataTheme} otter-dropdown-visible` },
+      { style: 'min-width: auto; left: -136px; top: -188px;' }
     );
     wrapper.setAttribute('data-index-number', items[order].id);
     createArrow();
     const itemContainer = makeHtmlElement(
       'ul', 
-      { class: 'otter-dropdown-menu otter-dropdown-menu-root otter-dropdown-menu-vertical otter-dropdown-menu-light' },
+      { class: `otter-dropdown-menu otter-dropdown-menu-root otter-dropdown-menu-vertical otter-dropdown-menu-${items[order].dataTheme}` },
       { role: 'menu' },
       { tabIndex: 0 }
-      // { dataMenuLsit : true }
     );
     itemContainer.setAttribute('data-menu-list', true);
     const groupOfItmes = [ 
@@ -64,21 +63,18 @@
         class: 'otter-dropdown-menu-item',
         role: 'menuitem',
         tabIndex: -1,
-        // dataMenuId : items[order].menu[0].id
       },
       { 
         id: 2, 
         class: 'otter-dropdown-menu-item otter-dropdown-menu-item-disabled',
         role: 'menuitem',
         tabIndex: -1,
-        // dataMenuId : items[order].menu[1].id
       },
       { 
         id: 3, 
         class: 'otter-dropdown-menu-item otter-dropdown-menu-item-danger',
         role: 'menuitem',
         tabIndex: -1,
-        // dataMenuId : items[order].menu[2].id
       },
     ]
     const [item1, item2, item3] = groupOfItmes.map((item) =>
@@ -139,6 +135,18 @@
       'use strict';
       createHtmlElement('div', 'otter-dropdown-arrow-light', 'otter-dropdown-show-arrow-light', 'otter-dropdown-show-arrow');
       createHtmlElement('div', 'otter-dropdown-arrow-black', 'otter-dropdown-show-arrow-black');
+      createHtmlElement('div', 'otter-dropdown-arrow-silver', 'otter-dropdown-show-arrow-silver');
+      createHtmlElement('div', 'otter-dropdown-arrow-gray', 'otter-dropdown-show-arrow-gray');
+      createHtmlElement('div', 'otter-dropdown-arrow-white', 'otter-dropdown-show-arrow-white');
+      createHtmlElement('div', 'otter-dropdown-arrow-maroon', 'otter-dropdown-show-arrow-maroon');
+      createHtmlElement('div', 'otter-dropdown-arrow-red', 'otter-dropdown-show-arrow-red');
+      createHtmlElement('div', 'otter-dropdown-arrow-purple', 'otter-dropdown-show-arrow-purple');
+      createHtmlElement('div', 'otter-dropdown-arrow-fuchsia', 'otter-dropdown-show-arrow-fuchsia');
+      createHtmlElement('div', 'otter-dropdown-arrow-green', 'otter-dropdown-show-arrow-green');
+      createHtmlElement('div', 'otter-dropdown-arrow-lime', 'otter-dropdown-show-arrow-lime');
+      createHtmlElement('div', 'otter-dropdown-arrow-olive', 'otter-dropdown-show-arrow-olive');
+      createHtmlElement('div', 'otter-dropdown-arrow-yellow', 'otter-dropdown-show-arrow-yellow');
+
       function createHtmlElement(element, class1, param1, param2) {
         'use strict';
         if (wrapper.classList.contains(param1) || wrapper.classList.contains(param2)) {
