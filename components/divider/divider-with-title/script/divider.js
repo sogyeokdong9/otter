509252479606f32ad08e6d2e3b1ec93559c79724
sogyeokdong9 <101911603+sogyeokdong9@users.noreply.github.com>
2022-05-08 08:00:01 +0900

    feat: Changed script, html & scss file
    Use `data-divider-specified="title"` data-attribute to indicate 'title'.

--- components/divider/divider-with-title/script/divider.js
+++ components/divider/divider-with-title/script/divider.js
@@ -2,15 +2,21 @@
 (function () {
   'use strict';
   const cntnrElmnt = document.querySelector('.wrap');
-  const trgElmn = cntnrElmnt.querySelectorAll('.otter-divider-with-text');
+  const trgElmn = cntnrElmnt.querySelectorAll('.otter-divider');
   const txtDivider = cntnrElmnt.querySelectorAll('.otter-divider-inner-text');
-
-  console.log(trgElmn);
-  console.log(txtDivider);
 
   for (let i = 0; i < trgElmn.length; i++) {
     const element = trgElmn[i];
     const txt = txtDivider[i];
+
+    if ( getDividerSpecified(i) === 'title' ) {
+      element.classList.add('otter-divider-with-text');
+    } else if ( getDividerSpecified(i) === 'plain') {
+      element.classList.add('otter-divider-with-text', 'otter-divider-plain' );
+    } else {
+      console.log(txt, `data-divider-specified="${getOrientation(i)}" is null.`);
+    }
+
     if ( getOrientation(i) === null && !checkOrientationMargin(i) ) {
       element.classList.add('otter-divider-with-text-center');
     } else if ( getOrientation(i) === 'left' ) {
@@ -30,16 +36,6 @@
         console.log(txt, `data-orientation="${getOrientation(i)}" is not supported.`);
       }
     }
-
-    // if ( getOrientation(i) !== null && getOrientationMargin(i) === '0' ) {
-    //   if ( getOrientation(i) === 'left' ) {
-    //     element.classList.add('otter-divider-no-default-orientation-margin-left');
-    //   } else if ( getOrientation(i) === 'right' ) {
-    //     element.classList.add('otter-divider-no-default-orientation-margin-right');
-    //   } else {
-    //     console.log(txt, `data-orientation="${getOrientation(i)}" is not supported.`);
-    //   }
-    // }
 
     if ( getOrientation(i) !== null && getOrientationMargin(i) !== '0' ) {
       if ( getOrientation(i) === 'left' ) {
@@ -72,6 +68,13 @@
     const element = trgElmn[idx];
     const hasOrientation = element.getAttribute('data-orient-margin');
     return Boolean(hasOrientation);
+  }
+
+  function getDividerSpecified(idx) {
+    'use strict';
+    const element = trgElmn[idx];
+    const dividerSpecified = element.getAttribute('data-divider-specified');
+    return dividerSpecified; 
   }
 
 })();
