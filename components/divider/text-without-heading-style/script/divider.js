b42fb7bce3d9021da74cf1ed27bac93f1b87bf1c
sogyeokdong9 <101911603+sogyeokdong9@users.noreply.github.com>
2022-05-08 13:23:20 +0900

    feat: Changed script, html & scss file
    👔 a36e0ee
    
    latest file synchronization(Divider: Vertical)
    
    index.html
    : Clean up code
    : Add script(divider.js)
    divider.js
    : Code Improvement
    divider.scss
    : Code Improvement

--- components/divider/text-without-heading-style/script/divider.js
+++ components/divider/text-without-heading-style/script/divider.js
@@ -8,7 +8,15 @@
   for (let i = 0; i < trgElmn.length; i++) {
     const element = trgElmn[i];
     const txt = txtDivider[i];
-
+    // divider type
+    if ( getDividerType(i) === null || getDividerType(i) === 'horizontal' ) {
+      element.classList.add('otter-divider-horizontal');
+    } else if ( getDividerType(i) === 'vertical') {
+      element.classList.add('otter-divider-vertical');
+    } else {
+      console.log( `getDividerType: horizontal(default), vertical / `, `data-divider-type="${getDividerType(i)}" is not supported.`);
+    }
+    // divider style
     if ( getDividerStyle(i) === 'solid' ) {
       element.classList.add('otter-divider-solid');
     } else if ( getDividerStyle(i) === 'dashed') {
@@ -16,17 +24,17 @@
     } else if ( getDividerStyle(i) === 'dotted') {
       element.classList.add('otter-divider-dotted');
     } else {
-      console.log(element, `data-divider-style="${getDividerStyle(i)}" is null.`);
+      console.log( `getDividerStyle: solid(default), dashed, dotted / `, `data-divider-style="${getDividerStyle(i)}" is null.` );
     }
-
+    // divider specified
     if ( getDividerSpecified(i) === 'title' ) {
       element.classList.add('otter-divider-with-text');
     } else if ( getDividerSpecified(i) === 'plain') {
       element.classList.add('otter-divider-with-text', 'otter-divider-plain' );
     } else {
-      console.log(txt, `data-divider-specified="${getDividerSpecified(i)}" is null.`);
+      console.log( `getDividerSpecified: (default), title, plain / `, `data-divider-specified="${getDividerSpecified(i)}" is null.` );
     }
-
+    // divider orientation
     if ( getOrientation(i) === null && !checkOrientationMargin(i) ) {
       element.classList.add('otter-divider-with-text-center');
     } else if ( getOrientation(i) === 'left' ) {
@@ -34,64 +42,63 @@
     } else if ( getOrientation(i) === 'right' ) {
       element.classList.add('otter-divider-with-text-right');
     } else {
-      console.log(txt, `data-orientation="${getOrientation(i)}" is not supported.`);
+      console.log( `getOrientation: center(default), left, right / `, `data-orientation="${getOrientation(i)}" is not supported.` );
     }
-
+    // divider orientation margin is not null
     if ( getOrientation(i) !== null && getOrientationMargin(i) !== null ) {
       if ( getOrientation(i) === 'left' ) {
         element.classList.add('otter-divider-no-default-orientation-margin-left');
       } else if ( getOrientation(i) === 'right' ) {
         element.classList.add('otter-divider-no-default-orientation-margin-right');
       } else {
-        console.log(txt, `data-orientation="${getOrientation(i)}" is not supported.`);
+        console.log( `getOrientation: center(default), left, right / `, `data-orientation="${getOrientation(i)}" is not supported.`);
       }
     }
-
+    // divider orientation margin is not '0'
     if ( getOrientation(i) !== null && getOrientationMargin(i) !== '0' ) {
       if ( getOrientation(i) === 'left' ) {
         txt.style.marginLeft = getOrientationMargin(i);
       } else if ( getOrientation(i) === 'right' ) {
         txt.style.marginRight = getOrientationMargin(i);
       } else {
-        console.log(txt, `data-orientation="${getOrientation(i)}" is not supported.`);
+        console.log( `getOrientation: center(default), left, right / `, `data-orientation="${getOrientation(i)}" is not supported.`);
       }
     }
-
   }
-
-  function getOrientation(idx) {
+  function getDividerType(idx) {
     'use strict';
     const element = trgElmn[idx];
-    const orientation = element.getAttribute('data-orientation');
-    return orientation;
+    const dividerType = element.getAttribute('data-divider-type');
+    return dividerType; 
   }
-
-  function getOrientationMargin(idx) {
-    'use strict';
-    const element = trgElmn[idx];
-    const orientationMargin = element.getAttribute('data-orientation-margin');
-    return orientationMargin;
-  }
-
-  function checkOrientationMargin(idx) {
-    'use strict';
-    const element = trgElmn[idx];
-    const hasOrientation = element.getAttribute('data-orient-margin');
-    return Boolean(hasOrientation);
-  }
-
-  function getDividerSpecified(idx) {
-    'use strict';
-    const element = trgElmn[idx];
-    const dividerSpecified = element.getAttribute('data-divider-specified');
-    return dividerSpecified; 
-  }
-
   function getDividerStyle(idx) {
     'use strict';
     const element = trgElmn[idx];
     const dividerStyle = element.getAttribute('data-divider-style');
     return dividerStyle; 
   }
-
+  function getDividerSpecified(idx) {
+    'use strict';
+    const element = trgElmn[idx];
+    const dividerSpecified = element.getAttribute('data-divider-specified');
+    return dividerSpecified; 
+  }
+  function getOrientation(idx) {
+    'use strict';
+    const element = trgElmn[idx];
+    const orientation = element.getAttribute('data-orientation');
+    return orientation;
+  }
+  function checkOrientationMargin(idx) {
+    'use strict';
+    const element = trgElmn[idx];
+    const hasOrientation = element.getAttribute('data-orient-margin');
+    return Boolean(hasOrientation);
+  }
+  function getOrientationMargin(idx) {
+    'use strict';
+    const element = trgElmn[idx];
+    const orientationMargin = element.getAttribute('data-orientation-margin');
+    return orientationMargin;
+  }
 })();
