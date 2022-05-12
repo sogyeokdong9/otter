4c718387fed231b66956b59818692395875c1b76
sogyeokdong9 <101911603+sogyeokdong9@users.noreply.github.com>
2022-05-12 23:25:28 +0900

    feat: Changed script, html & scss file
    1. index.html
    
    Add 'data attributes'
    : data-placement
    : data-message-type
    : data-massage-theme(TODO List)
    Change contents
    : Replace '&#8744;' to '<i class="ottericon gg-chevron-down"></i>'
    
    2. script.js
    
    Add function
    : getTrgAttrPlcmnt(idx)
    : createPlcmntClsNm(idx)
    : getDataMessagePlacement(idx)
    : getMessageTopOrBottom()
    Replace 'if...else if' to 'switch'
    : items[order].notice[0].type
    Change delay(value) of 'setTimeout()'.
    : setTimeout(() => { closeMessage() }, 3000);
    : setTimeout(() => { removeMessage(container) }, 300);
    To determine whether a message exists
    : Update the status information of the trigger based on the message that exists.
    
    3. animation.scss
    
    Add animation
    : otterMoveDownIn
    
    4. button.scss
    : Delete a file(Unnecessary)
    
    5. color.scss
    : Update contents(message)
    
    6. message.scss
    : Move contents(icon: 'reset.user.scss')
    : Add Class
    : Code Improvement
    
    7. reset.user.scss
    : Moved contents(icon)
    : @import url('https://css.gg/{icon}.css');

--- components/message/normal-prompt/script/script.js
+++ components/message/normal-prompt/script/script.js
@@ -4,15 +4,13 @@
   const cntnrElmnt = document.querySelector('.wrap');
   const trgElmn = cntnrElmnt.querySelectorAll('.otter-message-trigger');
   const message = document.querySelectorAll('.otter-message');
-  const logElmnt = cntnrElmnt.querySelector('.event-log-message');
-  const computedStyle = window.getComputedStyle(cntnrElmnt, null);
-
   document.querySelector(".otter-btn").addEventListener('click', function() {
     const items = [
       {
         "id":"1",
         "name":"Message",
         "dataTheme":"light",
+        "placement":"top",
         "notice":[
           {
             "id":"message-notice-div-tmp-key-0-0",
@@ -21,7 +19,9 @@
           }
         ]
       }
-    ]
+    ];
+    this.lastChild.previousElementSibling.ariaExpanded = true;
+    this.classList.replace('otter-message-close', 'otter-message-open');
     function returnCheckValue( val1, val2 = 'Message' ) {
       const result = val1 || val2;
       return result;
@@ -32,6 +32,7 @@
       getDataMessage(i);
       getDataMessageTheme(i);
       getDataMessageType(i);
+      getDataMessagePlacement(i);
       for (let j = 0; j < items[i].notice.length; j++) {
         const element = items[i].notice[j];
         items[i].notice[j].id = `${prefixTooltipId}-${i}-${j}`;
@@ -42,6 +43,22 @@
       'use strict';
       const element = trgElmn[idx];
       items[idx].notice[0].name = returnCheckValue(element.firstElementChild.dataset.message, items[idx].notice[0].name);
+    }
+    function getTrgAttrPlcmnt(idx) {
+      'use strict';
+      const element = trgElmn[idx];
+      const result = returnCheckValue(element.getAttribute('data-placement'), 'default');
+      return result;
+    }
+    function createPlcmntClsNm(idx) {
+      'use strict';
+      const prefixPlcmnt = 'otter-message-placement-';
+      const makePlcmntCls = prefixPlcmnt + getTrgAttrPlcmnt(idx);
+      return makePlcmntCls;
+    }
+    function getDataMessagePlacement(idx) {
+      'use strict';
+      return items[idx].placement = returnCheckValue(createPlcmntClsNm(idx), items[idx].placement);
     }
     function getDataMessageTheme(idx) {
       'use strict';
@@ -55,6 +72,7 @@
     }
     function createMessage(order,count) {
       'use strict';
+      message[order].classList.add(items[order].placement);
       const makeHtmlElement = function (tagName, ...attr) {
         const element = document.createElement(tagName);
         for (let prop of attr) {
@@ -68,82 +86,101 @@
         return element;
       };
       const container = makeHtmlElement(
-        'div', 
-        { class: `otter-message-notice otter-move-up-enter otter-move-up-enter-active` }
+        'div',
+        { class: `otter-message-notice` }
       );
       container.setAttribute('data-index-number', items[order].id);
-
+      function getMessageTopOrBottom() {
+        'use strict';
+        if ( getDataMessagePlacement(order).indexOf('top') === -1 ) {
+          const messagePlacement = 'bottom';
+          return messagePlacement;
+        } else {
+          const messagePlacement = 'top';
+          return messagePlacement;
+        }
+      }
+      if ( getMessageTopOrBottom() === "bottom") {
+        message[order].firstElementChild.classList.add('btm-basis');
+        container.classList.add('otter-move-down-enter', 'otter-move-down-enter-active')
+      } else {
+        message[order].firstElementChild.classList.add('top-basis');
+        container.classList.add('otter-move-up-enter', 'otter-move-up-enter-active')
+      }
       const wrapper = makeHtmlElement(
-        'div', 
-        { class: 'otter-message-notice-content' }, 
+        'div',
+        { class: 'otter-message-notice-content' },
         { role: 'noticeitem' }
       );
       wrapper.setAttribute('data-notice', true);
-
       const itemContainer = makeHtmlElement(
-        'div', 
+        'div',
         { class: 'otter-message-custom-content' }
       );
       itemContainer.setAttribute('data-notice-id', items[order].notice[0].id);
-  
       const span = makeHtmlElement( 'span' );
-  
       const icon = makeHtmlElement(
-        'i', 
+        'i',
         { class: 'ottericon' },
         { role: 'image' },
       );
-
-      if ( items[order].notice[0].type === "info" ) {
-        icon.classList.add("gg-info");
-        icon.setAttribute('aria-label', 'Information icon');
-        itemContainer.classList.add("otter-message-info");
-      } else if ( items[order].notice[0].type === "success" ) {
-        icon.classList.add("gg-check-o");
-        icon.setAttribute('aria-label', 'Success icon');
-        itemContainer.classList.add("otter-message-success");
-      } else if ( items[order].notice[0].type === "error" ) {
-        icon.classList.add("gg-close-o");
-        icon.setAttribute('aria-label', 'Error icon');
-        itemContainer.classList.add("otter-message-error");
-      } else if ( items[order].notice[0].type === "warning" ) {
-        icon.classList.add("gg-danger");
-        icon.setAttribute('aria-label', 'Warning icon');
-        itemContainer.classList.add("otter-message-warning");
-      } else if ( items[order].notice[0].type === "loading" ) {
-        icon.classList.add("gg-spinner");
-        icon.setAttribute('aria-label', 'Loading icon');
-        itemContainer.classList.add("otter-message-loading");
-      } else {
-        icon.classList.add("gg-bell");
-        icon.setAttribute('aria-label', 'Bell icon');
-        itemContainer.classList.add("otter-message-default");
+      const expr = items[order].notice[0].type;
+      switch (expr) {
+        case 'info':
+          icon.classList.add("gg-info");
+          icon.setAttribute('aria-label', 'Information icon');
+          itemContainer.classList.add("otter-message-info");
+          break;
+        case 'success':
+          icon.classList.add("gg-check-o");
+          icon.setAttribute('aria-label', 'Success icon');
+          itemContainer.classList.add("otter-message-success");
+          break;
+        case 'error':
+          icon.classList.add("gg-close-o");
+          icon.setAttribute('aria-label', 'Error icon');
+          itemContainer.classList.add("otter-message-error");
+          break;
+        case 'warning':
+          icon.classList.add("gg-danger");
+          icon.setAttribute('aria-label', 'Warning icon');
+          itemContainer.classList.add("otter-message-warning");
+          break;
+        case 'loading':
+          icon.classList.add("gg-spinner");
+          icon.setAttribute('aria-label', 'Loading icon');
+          itemContainer.classList.add("otter-message-loading");
+          break;
+        default:
+          icon.classList.add("gg-bell");
+          icon.setAttribute('aria-label', 'Bell icon');
+          itemContainer.classList.add("otter-message-default");
       }
-
       const spanContext = makeHtmlElement(
-        'span', 
+        'span',
         { textContent: items[order].notice[0].name }
       );
-  
       span.append(icon);
       itemContainer.append(span);
       itemContainer.append(spanContext);
       wrapper.append(itemContainer);
       container.append(wrapper);
-
       document.querySelector(".otter-message div").appendChild(container);
-
-      setTimeout(() => { closeMessage() }, 5000);
-
+      setTimeout(() => { closeMessage() }, 3000);
       function closeMessage() {
         'use strict';
         container.classList.replace('otter-move-up-enter', 'otter-move-up-leave');
         container.classList.replace('otter-move-up-enter-active', 'otter-move-up-leave-active');
-        setTimeout(() => { removeMessage(container) }, 1000);
+        setTimeout(() => { removeMessage(container) }, 300);
       }
       function removeMessage( target ) {
         'use strict'
+        const messageItem = document.querySelectorAll('.otter-message-notice');
         target.remove();
+        if ( messageItem.length === 1 ) {
+          trgElmn[order].classList.replace('otter-message-open', 'otter-message-close');
+          trgElmn[order].lastChild.previousElementSibling.ariaExpanded = false;
+        }
       }
     }
   })
